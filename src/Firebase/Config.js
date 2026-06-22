5b7b403f0a065be81930c53c5c7bcec8ef8e8135
Martinslopez <Martinslopez13@gmail.com>
2026-06-22 11:49:00 -0300

    Actualizando variables de entorno

--- src/Firebase/Config.js
+++ src/Firebase/Config.js
@@ -4,16 +4,16 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMdTZm6sToiAht9u_zwFVd3FoJGZAIA-w",
-  authDomain: "papelera-baires.firebaseapp.com",
-  projectId: "papelera-baires",
-  storageBucket: "papelera-baires.firebasestorage.app",
-  messagingSenderId: "725141248801",
-  appId: "1:725141248801:web:3c5aa9918e2723f2ae7c60",
-  measurementId: "G-35J1LXE3XQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app); 
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
